eef13afd6a412cdd0d95a09a9cf80263862dc5e2
Rupert Smith <rupert@thesett.com>
2018-09-03 14:56:50 +0100

    Building the CSS.

--- src/Utilities.elm
+++ src/Utilities.elm
@@ -1,7 +1,7 @@
 module Utilities exposing (..)
 
 import Array exposing (Array)
-import Css exposing (margin2, margin3, px)
+import Css exposing (..)
 
 
 baseFontSize =
@@ -46,6 +46,114 @@ rhythm n =
     px <| n * baseLineHeight
 
 
+
+-- Reset
+
+
+{-| No margin or padding.
+
+Default on:
+
+blockquote, caption, dd, dl, fieldset, form, h1, h2, h3, h4, h5, h6, hr, legend,
+ol, p, pre, table, td, th, ul
+
+-}
+noMarginOrPadding =
+    [ margin <| px 0
+    , padding <| px 0
+    ]
+
+
+{-| Remove underlines from potentially troublesome elements.
+
+Default on:
+
+a, ins, u
+
+-}
+noDecoration =
+    textDecoration none
+
+
+{-| Apply faux underline via `border-bottom`.
+
+Default on:
+
+ins
+
+-}
+fauxUnderline =
+    borderBottom2 (px 1) solid
+
+
+{-| So that `alt` text is visually offset if images dont load.
+
+Default on:
+
+img
+
+-}
+italicAltText =
+    fontStyle italic
+
+
+{-| Remove borders from fieldset
+
+Default on:
+
+fieldset
+
+-}
+borderless =
+    border2 (px 0) none
+
+
+{-| Give form elements some cursor interactions...
+
+Default on:
+
+button, input:button, label, option, select
+
+-}
+defaultCursor =
+    cursor pointer
+
+
+{-| Default cursor and box style for text inputs. }
+
+Default on:
+
+.text-input:active, .text-input:focus, textarea:active, textarea:focus
+
+-}
+defaultTextInput =
+    [ cursor text_, outline none ]
+
+
+
+-- Responsive Values
+
+
+screenXs =
+    px 480
+
+
+screenSm =
+    px 840
+
+
+screenMd =
+    px 960
+
+
+screenLg =
+    px 1280
+
+
+
+-- Base Spacing
+
+
 {-| Single direction margins.
 
 Use this for:
@@ -76,11 +184,5 @@ dd, ol, ul
 
 -}
 listMargin =
+    --     margin-left: pem(2 * $base-spacing-unit);
     margin2 (px <| 2 * baseSpacingUnit) (px <| 2 * baseSpacingUnit)
-
-
-
--- {
---     margin-left: (2 * $base-spacing-unit);
---     margin-left: pem(2 * $base-spacing-unit);
--- }
