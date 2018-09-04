d1e1fb4a92fb6011e1c77cbfe9efb12fa327b624
Rupert Smith <rupert@thesett.com>
2018-09-04 16:50:53 +0100

    Pulled together all of the global styling.

--- src/TheSettLaf.elm
+++ src/TheSettLaf.elm
@@ -1,17 +1,30 @@
-module TheSettLaf exposing (x)
+module TheSettLaf exposing (snippets, style)
 
 {-| The Sett Look and Feel
 
-@docs x
+@docs snippets, style
 
 -}
 
-import Mixins
+import Css
+import Css.Foreign
+import Html.Styled exposing (Html)
 import Utilities
 import Quarks
 
 
-{-| -}
-x : number
-x =
-    1
+{-| The CSS as an HTML <style> element.
+-}
+style : Html msg
+style =
+    Css.Foreign.global snippets
+
+
+{-| The global CSS.
+-}
+snippets : List Css.Foreign.Snippet
+snippets =
+    Utilities.reset
+        ++ Utilities.normalize
+        ++ Utilities.baseSpacing
+        ++ Quarks.headings
