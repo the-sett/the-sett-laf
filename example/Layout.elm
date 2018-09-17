a1dcfc040b0abd3f27410ea1e4c33834610d9fe2
Rupert Smith <rupert@thesett.com>
2018-09-17 14:57:00 +0100

    Added some padding to the menu.

--- example/Layout.elm
+++ example/Layout.elm
@@ -52,15 +52,15 @@ topHeader devices model =
                     [ styled li
                         [ Css.display Css.inline ]
                         []
-                        [ text "Typography" ]
+                        [ styled a [ Css.padding (Css.px 10) ] [] [ text "Typography" ] ]
                     , styled li
                         [ Css.display Css.inline ]
                         []
-                        [ text "Buttons" ]
+                        [ styled a [ Css.padding (Css.px 10) ] [] [ text "Buttons" ] ]
                     , styled li
                         [ Css.display Css.inline ]
                         []
-                        [ text "Etc" ]
+                        [ styled a [ Css.padding (Css.px 10) ] [] [ text "Etc" ] ]
                     ]
                 ]
             , div []
