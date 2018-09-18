a18f0fbd1b7b5e7ae67cd914ec8983536e683850
Rupert Smith <rupert@thesett.com>
2018-09-18 17:01:50 +0100

    Added a card function.

--- example/src/elm/Cards.elm
+++ example/src/elm/Cards.elm
@@ -4,6 +4,7 @@ import Css
 import Html.Styled exposing (..)
 import Html.Styled.Attributes exposing (title, class, name, src)
 import Structure exposing (Template)
+import Responsive exposing (deviceStyle, rhythm)
 
 
 view : Template msg model
@@ -19,58 +20,45 @@ view devices model =
             []
             []
             [ div [ class "mdl-grid" ]
-                [ div [ class "mdl-cell mdl-cell--6-col mdl-cell--4-col-tablet mdl-cell--4-col-phone mdl-card mdl-shadow--3dp" ]
-                    [ div [ class "mdl-card__media" ]
-                        [ img [ src "images/more-from-4.png" ]
-                            []
-                        ]
-                    , div [ class "mdl-card__title" ]
-                        [ h4 [ class "mdl-card__title-text" ]
-                            [ text "Card 2" ]
-                        ]
-                    , div [ class "mdl-card__supporting-text" ]
-                        [ p []
-                            [ text "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " ]
-                        ]
-                    , div [ class "mdl-card__actions" ]
-                        [--  Button.render Mdl
-                         --     [ 0 ]
-                         --     model.mdl
-                         --     [ Button.colored
-                         --     , Button.ripple
-                         --     ]
-                         --     [ text "Link"
-                         --     , i [ class "material-icons" ]
-                         --         [ text "chevron_right" ]
-                         --     ]
-                        ]
-                    ]
-                , div [ class "mdl-cell mdl-cell--6-col mdl-cell--4-col-tablet mdl-cell--4-col-phone mdl-card mdl-shadow--3dp" ]
-                    [ div [ class "mdl-card__media" ]
-                        [ img [ src "images/more-from-3-large.png" ]
-                            []
-                        ]
-                    , div [ class "mdl-card__title" ]
-                        [ h4 [ class "mdl-card__title-text" ]
-                            [ text "Card 4" ]
-                        ]
-                    , div [ class "mdl-card__supporting-text" ]
-                        [ p []
-                            [ text "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " ]
-                        ]
-                    , div [ class "mdl-card__actions" ]
-                        [-- Button.render Mdl
-                         --     [ 1 ]
-                         --     model.mdl
-                         --     [ Button.colored
-                         --     , Button.ripple
-                         --     ]
-                         --     [ text "Link"
-                         --     , i [ class "material-icons" ]
-                         --         [ text "chevron_right" ]
-                         --     ]
-                        ]
-                    ]
+                [ card devices "Card1" "images/more-from-4.png"
+                , card devices "Card2" "images/more-from-3.png"
                 ]
+            ]
+        ]
+
+
+card devices title imgSrc =
+    div [ class "mdl-cell mdl-cell--6-col mdl-cell--4-col-tablet mdl-cell--4-col-phone mdl-card mdl-shadow--3dp" ]
+        [ styled div
+            [ deviceStyle devices <|
+                \deviceProps -> Css.height (rhythm deviceProps 6)
+            ]
+            [ class "mdl-card__media" ]
+            [ styled img
+                [ Css.maxHeight (Css.pct 100)
+                , Css.width (Css.pct 100)
+                ]
+                [ src imgSrc ]
+                []
+            ]
+        , div [ class "mdl-card__title" ]
+            [ h4 [ class "mdl-card__title-text" ]
+                [ text title ]
+            ]
+        , div [ class "mdl-card__supporting-text" ]
+            [ p []
+                [ text "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " ]
+            ]
+        , div [ class "mdl-card__actions" ]
+            [--  Button.render Mdl
+             --     [ 0 ]
+             --     model.mdl
+             --     [ Button.colored
+             --     , Button.ripple
+             --     ]
+             --     [ text "Link"
+             --     , i [ class "material-icons" ]
+             --         [ text "chevron_right" ]
+             --     ]
             ]
         ]
