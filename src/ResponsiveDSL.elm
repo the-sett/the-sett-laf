c33fb177b3ba420afa764f6597c5c27fd4af77e2
rupertlssmith <github@badgerhouse.freeserve.co.uk>
2018-11-18 17:11:52 +0000

    Documentation and version bump for new build.

--- src/ResponsiveDSL.elm
+++ src/ResponsiveDSL.elm
@@ -1,10 +1,22 @@
 module ResponsiveDSL exposing
-    ( sm, md, lg, xl
-    , styles
-    , Builder(..), Compatible(..), ContainerBuilder, DeviceBuilder, ElementBuilder, StyleBuilder, applyDevice, applyDevicesToBuilders, empty
+    ( Compatible(..)
+    , Builder(..), ContainerBuilder, DeviceBuilder, ElementBuilder, StyleBuilder
+    , sm, md, lg, xl
+    , styles, empty
+    , applyDevice, applyDevicesToBuilders
     )
 
 {-|
+
+
+# Compatability shadow type for ensuring correct DSL construction.
+
+@docs Compatible
+
+
+# Builder types.
+
+@docs Builder, ContainerBuilder, DeviceBuilder, ElementBuilder, StyleBuilder
 
 
 # Device builders
@@ -12,9 +24,14 @@ module ResponsiveDSL exposing
 @docs sm, md, lg, xl
 
 
-# Inject any CSS styles
+# Injecting CSS styles
 
-@docs styles
+@docs styles, empty
+
+
+# For applying responsive devices.
+
+@docs applyDevice, applyDevicesToBuilders
 
 -}
 
@@ -37,14 +54,20 @@ import Responsive
 -- Responsive style builders.
 
 
+{-| Used as a shadow type to ensure correct DSL construction.
+-}
 type Compatible
     = Compatible
 
 
+{-| A builder for a device in a DSL context.
+-}
 type Builder a ctx
     = Builder Device ctx (ctx -> BaseStyle -> List Css.Style)
 
 
+{-| Builds a container of Elements. This can be styled per device.
+-}
 type alias ContainerBuilder a ctx msg =
     List (List (ctx -> Builder a ctx))
     -> List (Attribute msg)
@@ -53,6 +76,8 @@ type alias ContainerBuilder a ctx msg =
     -> Html msg
 
 
+{-| Builds an Element. This can be styled per device.
+-}
 type alias ElementBuilder a ctx msg =
     List (List (ctx -> Builder a ctx))
     -> List (Attribute msg)
@@ -61,10 +86,14 @@ type alias ElementBuilder a ctx msg =
     -> Html msg
 
 
+{-| Builds styles per responsive Device.
+-}
 type alias DeviceBuilder a ctx =
     List (Device -> ctx -> Builder a ctx) -> List (ctx -> Builder a ctx)
 
 
+{-| Builds a style within a Context.
+-}
 type alias StyleBuilder a ctx =
     Device -> ctx -> Builder a ctx
 
@@ -76,11 +105,15 @@ styles styleList device ctx =
     Builder device ctx (always2 styleList)
 
 
+{-| Applies a responsive device to a list of StyleBuilders.
+-}
 applyDevice : Device -> List (Device -> ctx -> Builder a ctx) -> List (ctx -> Builder a ctx)
 applyDevice device builders =
     List.map (\buildFn -> buildFn device) builders
 
 
+{-| Applies a set of responsive device specifications to a list of builders, in any context.
+-}
 applyDevicesToBuilders : List (Builder a ctx) -> DeviceStyles -> Css.Style
 applyDevicesToBuilders buildersList devices =
     deviceStyles devices
@@ -98,6 +131,8 @@ applyDevicesToBuilders buildersList devices =
         )
 
 
+{-| An empty style, for convenience when sketching out DSLs.
+-}
 empty : StyleBuilder a ctx
 empty =
     \device ctx -> Builder device ctx (always2 [])
