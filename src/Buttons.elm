ef5865a9393671d650ff52a9d14a36e42f17c220
Rupert Smith <rupert@thesett.com>
2018-11-20 16:49:15 +0000

    Trying to reduce the button style down to what is common to all buttons.

--- src/Buttons.elm
+++ src/Buttons.elm
@@ -50,8 +50,7 @@ button builders attributes innerHtml devices =
         , Css.borderStyle Css.none
         , Css.borderRadius <| Css.px 2
         , Css.color <| Css.rgb 0 0 0
-        , Css.margin <| Css.px 24
-        , Css.padding2 (Css.px 0) (Css.em 1)
+        , Css.padding2 (Css.px 0) (Css.em 0.8)
         , Css.textTransform Css.uppercase
         , Css.overflow Css.hidden
         , Css.outline Css.none
@@ -59,6 +58,8 @@ button builders attributes innerHtml devices =
         , Css.verticalAlign Css.middle
 
         --, Css.batch [ TypeScale.fontMediaStyles devices scale TypeScale.milli ]
+        -- , Css.fontSize <| Css.em 1.0
+        -- , Css.lineHeight <| Css.em devices.commonStyle.lineHeightRatio -- * (1.0 / 0.7))
         -- Raised
         , Css.backgroundColor <| Css.rgba 158 158 158 0.2
         , Css.property "box-shadow" "0 2px 2px 0 rgba(0,0,0,.14), 0 3px 1px -2px rgba(0,0,0,.2), 0 1px 5px 0 rgba(0,0,0,.12)"
